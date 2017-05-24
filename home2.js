d02ff37c24d1b35c5df7f76ac4ae6a7d666bbec8
Zeeshan Ansari <zansari626@gmail.com>
2017-05-24 19:08:34 +0530

    Website

---
+++ home2.js
@@ -0,0 +1,9 @@
+document.getElementById("nav01").innerHTML = 
+"<ul id='menu'>"+
+"<li><a href='home.html'>HOME </a></li>"+
+"<li><a href='gallery.html'>GALLERY </a></li>"+
+"<li><a href='music.html'>ONLINE VIDEO MUSIC </a></li>"+
+"<li><a href='musiconline.html'>ONLINE AUDIO MUSIC </a></li>"+
+"<li><a href='form.html'>CONTACT US </a></li>"+
+"<li><a href='about.html'>ABOUT US </a></li>"+
+"</ul>";
