bdf4235c9d8af60fb5b1462290a328831593e28b
Zeeshan Ansari <zansari626@gmail.com>
2018-09-13 18:53:25 +0530

    Update home2.js
    Removed one link from header

--- home2.js
+++ home2.js
@@ -2,7 +2,6 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>"+
 "<li><a href='index.html'>HOME </a></li>"+
 "<li><a href='gallery.html'>GALLERY </a></li>"+
-"<li><a href='music.html'>ONLINE VIDEO MUSIC </a></li>"+
 "<li><a href='musiconline.html'>ONLINE AUDIO MUSIC </a></li>"+
 "<li><a href='form.html'>CONTACT US </a></li>"+
 "<li><a href='about.html'>ABOUT US </a></li>"+
