5d9849cfbf4c60cec439c6d65523fa2e1bef3a3f
Zeeshan Ansari <zansari626@gmail.com>
2018-09-13 19:25:47 +0530

    Update home2.js
    Name Update

--- home2.js
+++ home2.js
@@ -2,7 +2,7 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>"+
 "<li><a href='index.html'>HOME </a></li>"+
 "<li><a href='gallery.html'>GALLERY </a></li>"+
-"<li><a href='musiconline.html'>ONLINE AUDIO MUSIC </a></li>"+
+"<li><a href='musiconline.html'>MUSIC GALLERY </a></li>"+
 "<li><a href='form.html'>CONTACT US </a></li>"+
 "<li><a href='about.html'>ABOUT US </a></li>"+
 "</ul>";
