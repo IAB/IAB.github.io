7fd31a6a1219aa63f75c66d30ffa6dad559b7bff
Zeeshan Ansari <zansari626@gmail.com>
2018-09-14 14:44:10 +0530

    Update home2.js
    Gallery Removed

--- home2.js
+++ home2.js
@@ -1,7 +1,6 @@
 document.getElementById("nav01").innerHTML = 
 "<ul id='menu'>"+
 "<li><a href='index.html'>HOME </a></li>"+
-"<li><a href='gallery.html'>GALLERY </a></li>"+
 "<li><a href='musiconline.html'>MUSIC GALLERY </a></li>"+
 "<li><a href='form.html'>CONTACT US </a></li>"+
 "<li><a href='about.html'>ABOUT US </a></li>"+
